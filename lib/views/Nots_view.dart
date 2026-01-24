9e9baa3151e6e9a25469d2e3acae5c4b2daabbf0
abod_hesahm <egalsclub5@gmail.com>
2026-01-24 22:12:41 +0200

    feature app bar and create custtom search icon

--- lib/views/Nots_view.dart
+++ lib/views/Nots_view.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:nots_app/widgets/search_icon.dart';
 
 class NotsView extends StatelessWidget {
   const NotsView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        actions: [SearchIcon()],
+        title: Text(
+          'CNotes',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'PlaywriteGBJGuides',
+            fontStyle: FontStyle.italic,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 10),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.white,
+          ),
+          width: double.infinity,
+          height: 180,
+        ),
+      ),
+    );
   }
 }
---
+++ lib/widgets/search_icon.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class SearchIcon extends StatelessWidget {
+  const SearchIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 8.0),
+      child: Container(
+        width: 45,
+        height: 45,
+        decoration: BoxDecoration(
+          color: Colors.black.withOpacity(.4),
+          borderRadius: BorderRadius.circular(16),
+        ),
+
+        child: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.search),
+          iconSize: 22,
+        ),
+      ),
+    );
+  }
+}
