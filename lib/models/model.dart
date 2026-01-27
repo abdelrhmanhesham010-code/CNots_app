24892f46e93ec11785b81ecee5e9dcb36abf656e
abod_hesahm <egalsclub5@gmail.com>
2026-01-27 22:45:59 +0200

    create model

---
+++ lib/models/model.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class NoteMode {
+  final String title;
+  final String subTitle;
+  final String date;
+  final Color color;
+  NoteMode(this.title, this.subTitle, this.date, this.color);
+}
--- lib/widgets/custom_bottom.dart
+++ lib/widgets/custom_bottom.dart
@@ -5,20 +5,23 @@ class custombottom extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Color(0xffBFECFF),
-        borderRadius: BorderRadius.circular(8),
-      ),
-      width: MediaQuery.of(context).size.width,
-      height: 50,
-      child: Center(
-        child: Text(
-          'Add',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        decoration: BoxDecoration(
+          color: Color(0xffBFECFF),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        width: MediaQuery.of(context).size.width,
+        height: 50,
+        child: Center(
+          child: Text(
+            'Add',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
       ),
