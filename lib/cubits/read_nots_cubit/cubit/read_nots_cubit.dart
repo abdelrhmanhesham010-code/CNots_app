66583be0bee39d70c14d54de0426c5dbfb76d91e
abod_hesahm <egalsclub5@gmail.com>
2026-02-01 19:44:20 +0200

    create fetch_nots method

--- lib/cubits/read_nots_cubit/cubit/read_nots_cubit.dart
+++ lib/cubits/read_nots_cubit/cubit/read_nots_cubit.dart
@@ -1,9 +1,20 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
+import 'package:nots_app/constant/constant.dart';
 import 'package:nots_app/models/note_model.dart';
 
 part 'read_nots_state.dart';
 
 class ReadNotsCubit extends Cubit<ReadNotsState> {
   ReadNotsCubit() : super(ReadNotsInitial());
+  fetcAllhNote() async {
+    try {
+      var notsBox = Hive.box<NoteModel>(kPnoteBox);
+      List<NoteModel> notes = notsBox.values.toList();
+      emit(ReadNotsSucess(notes));
+    } catch (e) {
+      emit(ReadNotsFaliuer(e.toString()));
+    }
+  }
 }
--- lib/views/Nots_view.dart
+++ lib/views/Nots_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nots_app/cubits/read_nots_cubit/cubit/read_nots_cubit.dart';
 import 'package:nots_app/widgets/floating_button_note.dart';
 import 'package:nots_app/widgets/nots_list_view.dart';
 import 'package:nots_app/widgets/search_icon.dart';
@@ -8,37 +10,40 @@ class NotsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadiusGeometry.circular(20),
+    return BlocProvider(
+      create: (context) => ReadNotsCubit(),
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadiusGeometry.circular(20),
+              ),
+              context: context,
+              builder: (context) {
+                return AddNoteButton();
+              },
+            );
+          },
+          child: Icon(Icons.add),
+        ),
+        appBar: AppBar(
+          actions: [SearchIcon(icon: Icon(Icons.search))],
+          backgroundColor: Color(0xff6A1E55),
+          title: const Text(
+            'CNotes',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'PlaywriteGBJGuides',
+              fontStyle: FontStyle.italic,
             ),
-            context: context,
-            builder: (context) {
-              return AddNoteButton();
-            },
-          );
-        },
-        child: Icon(Icons.add),
-      ),
-      appBar: AppBar(
-        actions: [SearchIcon(icon: Icon(Icons.search))],
-        backgroundColor: Color(0xff6A1E55),
-        title: const Text(
-          'CNotes',
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-            fontFamily: 'PlaywriteGBJGuides',
-            fontStyle: FontStyle.italic,
           ),
         ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.only(top: 10),
-        child: NotsListView(),
+        body: Padding(
+          padding: const EdgeInsets.only(top: 10),
+          child: NotsListView(),
+        ),
       ),
     );
   }
